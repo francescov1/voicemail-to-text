61316085128bbffe5afed4d16aa7a6e6e9a11dda
Francesco Virga <francescod.virga@gmail.com>
2019-07-04 16:13:00 -0400

    Setup parsing

--- helpers/dialog.js
+++ helpers/dialog.js
@@ -1,7 +1,4 @@
 'use strict';
-const config = require('../config/index');
-const client = require('../config/twilio');
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 // voicemail breakdown (rogers)
 
@@ -39,24 +36,50 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 // dialog: "{message}"
 // continue same flow as above
 
-exports.accessVoicemail = async (number, message) => {
-  const voice = new VoiceResponse();
+// type can be "new" or "saved"
+const parseMessages = (text, type) => {
 
-  // initiate the call
-  const call = await client.calls.create({
-    url: `${config.base_url}/api/voice/initialCallHandler`,
-    to: number,
-    from: config.twilio.sender_id,
-    statusCallback: `${config.base_url}/api/voice/statusCallBack`,
-    statusCallbackMethod: 'POST'
-  });
+  let parsedMessages = { messages: [], n: 0 };
 
-  // press pound key when call begins
-  voice.play({
-    digits: '#'
-  });
+  // split into [everything before, n, all messages]
+  let allMessages = text.split(
+    new RegExp(`([0-9]+) ${type === "saved" ? "saved" : "new wireless voice"} messages?`)
+  );
 
-  console.log(voice.toString());
+  if (allMessages.length === 1) {
+    console.log(`no ${type} messages`)
+    return parsedMessages;
+  }
+  else {
+    parsedMessages.n = allMessages[1];
+  }
 
-  return Promise.resolve();
+  if (type === "saved") {
+    text = text.replace(/.*first saved message/, '');
+  }
+  else {
+    text = text.replace(/.*first new message/, '');
+    text = text.replace(/first saved message.*/, '');
+  }
+
+  // split messages into array of voicemails
+  let messages = text.split('next saved message')
+
+  parsedMessages.messages = messages.map(msg => {
+    return msg.replace('to erase this message, press 7, to reply to it, press 8, to save it, press 9', '');
+  })
+
+  return parsedMessages;
+}
+
+exports.parseVoicemail = (text) => {
+  // TODO: double check if need to lowercase text
+  let voicemails = {};
+
+  // remove anything after messages
+  text = text.replace(/end of messages.*/, '')
+  voicemails.saved = parseMessages(text, "saved")
+  voicemails.new = parseMessages(text, "new");
+
+  return voicemails;
 }
