68c52799a608f6a5ebf15dcf012f5f9e471394bf
Francesco Virga <francescod.virga@gmail.com>
2019-07-04 13:23:16 -0400

    Setup switch for commands

--- routes/sms.js
+++ routes/sms.js
@@ -7,7 +7,7 @@ const client = require('../config/twilio');
 
 router.post('/receive', async (req, res, next) => {
   const number = req.body.From;
-  const message = req.body.Body;
+  const message = req.body.Body.toLowerCase().trim().split(' ');
 
   // TODO: check whos number, may nee to adjust the speech to text based
   // on which service provider
@@ -16,6 +16,21 @@ router.post('/receive', async (req, res, next) => {
 
   // TODO: decide on certain keywords for actions from voicemail
   // such as read voicemails, delete voicemail
+  let response;
+  switch (message[0]) {
+    case "read":
+      // call number, get voicemail, relay back
+      response = `Number of voicemails: {n_voicemails}\n\n1 - Left from {sender_number}\nMessage: {message}`
+      break;
+    case "delete":
+      // call number, get voicemail, loop through rest of message array and delete
+      // numbers provided
+      response = `Voicemails ${message.slice(0)} deleted successfully.`
+      break;
+    default:
+      response = `'${message[0]}' is an invalid command.\n\nAvailable commands:\READ - Reply with all voicemails\DELETE {n1} {n2} ... - Delete voicemails'`
+      break;
+  }
 
   // number each voicemail so user can text back which
   // voicemail to delete
@@ -24,7 +39,7 @@ router.post('/receive', async (req, res, next) => {
 
   try {
     const message = await client.messages.create({
-      body: `Relaying voicemail back to you!`,
+      body: response,
       from: config.twilio.sender_id,
       to: number
     });
