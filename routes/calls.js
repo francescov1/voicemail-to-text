1e655d4bc79d31f6f60e937ba7c546da78ad100f
Francesco Virga <francescod.virga@gmail.com>
2023-04-08 22:24:36 -0600

    Add try catches

--- routes/calls.js
+++ routes/calls.js
@@ -9,66 +9,73 @@ const router = express.Router();
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 router.post('/initialCallHandler', async (req, res, next) => {
-  const { number, message } = req.query;
-  console.log("Received initial call handler callback, responding with TwiML")
-  console.log('number: ' + number);
-  console.log('message: ' + message);
+  try {
+    const { number, message } = req.query;
+    console.log("Received initial call handler callback, responding with TwiML")
+    console.log('number: ' + number);
+    console.log('message: ' + message);
 
-  const response = new VoiceResponse();
+    const response = new VoiceResponse();
 
-  response.pause({ length: 10 });
+    response.pause({ length: 5 });
 
-  // NOTE: Can add "w" between digits for a 0.5 second pause
-  response.play({ digits: number.slice(-10) });
+    // NOTE: Can add "w" between digits for a 0.5 second pause
+    response.play({ digits: number.slice(-10) });
 
-  response.pause({ length: 10 });
+    response.pause({ length: 10 });
 
-  response.play({ digits: config.voice_password });
+    response.play({ digits: config.voice_password });
 
-  // record the voicemail and send for parsing
-  const record = response.record({
-    // TODO: This implementation is messy, clean up. Essentially were getting the command "read" or "delete" from the API call and then using it as the route path
-    action: `${config.base_url}/call/${message.split(' ')[0]}?number=${number}`,
-    method: 'POST',
-    timeout: 20,
-    maxLength: 7200,
-    transcribe: true,
-    transcribeCallback: `${config.base_url}/call/${message.split(' ')[0]}?number=${number}`,
-  });
+    const transcriptionCallbackUrl = `${config.base_url}/call/${message.split(' ')[0]}?number=${number}`
+    console.log("Sending recording transcription callback to URL " + transcriptionCallbackUrl);
 
-  res.type('text/xml');
-  res.send(response.toString());
+    // record the voicemail and send for parsing
+    const record = response.record({
+      // TODO: This implementation is messy, clean up. Essentially were getting the command "read" or "delete" from the API call and then using it as the route path
+      action: transcriptionCallbackUrl,
+      method: 'POST',
+      timeout: 20,
+      maxLength: 7200,
+      transcribe: true,
+      transcribeCallback: transcriptionCallbackUrl
+    });
+
+    res.type('text/xml');
+    res.send(response.toString());
+  }
+  catch(err) {
+    return next(err)
+  }
 });
 
 router.post('/read', async (req, res, next) => {
-  if (!req.body.TranscriptionUrl) {
-    // This gets called when the recording completes but before the transcript is done
-    console.log('No transcript URL present, ignoring request')
-    return res.end();
-  }
-
-  const voicemailDialog = req.body.TranscriptionText;
-  const number = req.query.number;
-
-  console.log('Parsing voicemail dialog: ', voicemailDialog);
-  const voicemails = dialog.parseVoicemail(voicemailDialog);
-
-  // TODO: get sender
-  let response = `New voicemails: ${voicemails.new.n}\n`;
-  let counter = 1;
-  for (let msg of voicemails.new.messages) {
-    response += `\n${counter} - ${msg}`;
-    counter++;
-  }
-
-  response += `\n\nSaved voicemails: ${voicemails.saved.n}\n`;
-
-  for (let msg of voicemails.saved.messages) {
-    response += `\n${counter} - ${msg}`;
-    counter++;
-  }
-
   try {
+    if (!req.body.TranscriptionUrl) {
+      // This gets called when the recording completes but before the transcript is done
+      console.log('No transcript URL present, ignoring request')
+      return res.end();
+    }
+
+    const voicemailDialog = req.body.TranscriptionText;
+    const number = req.query.number;
+
+    console.log('Parsing voicemail dialog: ', voicemailDialog);
+    const voicemails = dialog.parseVoicemail(voicemailDialog);
+
+    // TODO: get sender
+    let response = `New voicemails: ${voicemails.new.n}\n`;
+    let counter = 1;
+    for (let msg of voicemails.new.messages) {
+      response += `\n${counter} - ${msg}`;
+      counter++;
+    }
+
+    response += `\n\nSaved voicemails: ${voicemails.saved.n}\n`;
+
+    for (let msg of voicemails.saved.messages) {
+      response += `\n${counter} - ${msg}`;
+      counter++;
+    }
     const message = await client.messages.create({
       body: response,
       from: config.twilio.sender_id,
@@ -82,11 +89,17 @@ router.post('/read', async (req, res, next) => {
 
 // TODO:
 router.post('/delete', async (req, res, next) => {
-  const voicemailDialog = req.body.dialog;
-  const number = req.body.number;
-  const voicemailsToDelete = req.body.delete;
+  try {
 
-  const voicemails = dialog.parseMessages(voicemailDialog);
+    const voicemailDialog = req.body.dialog;
+    const number = req.body.number;
+    const voicemailsToDelete = req.body.delete;
+    
+    const voicemails = dialog.parseMessages(voicemailDialog);
+  }
+  catch(err) {
+    return next(err)
+  }
 });
 
 module.exports = router;
--- routes/sms.js
+++ routes/sms.js
@@ -11,24 +11,29 @@ const dialog = require('../helpers/dialog');
 // TODO: Look at price for every execution, decide if its worth checking regularly, or just leave it up to user to initiate via sms
 
 router.post('/getVoicemail', async (req, res, next) => {
-  // remove '+' from number
-  const number = req.body.From.slice(1);
+  try {
+    // remove '+' from number
+    const number = req.body.From.slice(1);
 
-  // TODO: To throw error, need to send an SMS message back to user.
-  if (req.body.From !== config.personal_number) {
-    throw new Error("Invalid sender")
+    // TODO: To throw error, need to send an SMS message back to user.
+    if (req.body.From !== config.personal_number) {
+      throw new Error("Invalid sender")
+    }
+
+    const command = req.body.Body.toLowerCase().trim();
+
+    if (!/^read$|^delete( [0-9]{1,2})+/.test(command)) {
+      throw new Error("Invalid message format. Please use 'read' or 'delete {n1} {n2} ...'")
+    }
+    
+    const message = command.split(' ');
+
+    await calls.accessVoicemail(number, message);
+    return res.end();
   }
-
-  const command = req.body.Body.toLowerCase().trim();
-
-  if (!/^read$|^delete( [0-9]{1,2})+/.test(command)) {
-    throw new Error("Invalid message format. Please use 'read' or 'delete {n1} {n2} ...'")
+  catch(err) {
+    return next(err)
   }
-  
-  const message = command.split(' ');
-
-  await calls.accessVoicemail(number, message);
-  return res.end();
 });
 
 module.exports = router;
