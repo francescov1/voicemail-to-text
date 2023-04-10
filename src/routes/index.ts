15225a01738431766f2ca7ce9f4d3df34e158218
Francesco Virga <francescod.virga@gmail.com>
2023-04-10 10:34:27 -0600

    Fix router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import smsRouter from './sms'
+import callsRouter from './calls'
+
 const router = express.Router();
 
-router.use('/sms', require('./sms'));
-router.use('/call', require('./calls'));
+router.use('/sms', smsRouter);
+router.use('/call', callsRouter);
 
 export default router;
